d27781865b3aa2ffe09f43d5c2fa36efaf5b1b90
BogdanGrynjuk <bgrynjuk@ukr.net>
2025-01-07 00:56:03 +0200

    Add styles for SettingsQRCode

--- src/components/SettingsQRCode/SettingsQRCode.tsx
+++ src/components/SettingsQRCode/SettingsQRCode.tsx
@@ -18,7 +18,7 @@ const SettingsQRCode: React.FC = () => {
     size: '100',
     border: '0',
   });
-  const [qrCodeUrl, setQrCodeUrl] = useState<string | null>(null);
+
   const isDisabled = !formData.data;
   const counter = formData.data.length;
 
@@ -42,7 +42,6 @@ const SettingsQRCode: React.FC = () => {
     try {
       const response = await fetch(fullUrl);
       if (response.status === 200) {
-        setQrCodeUrl(fullUrl);
         console.log(fullUrl);
       } else {
         console.error('Не вдалося згенерувати QR-код');
@@ -67,100 +66,105 @@ const SettingsQRCode: React.FC = () => {
 
   return (
     <section className={css.container}>
-      <header>
-        <h2>Generate your QR code</h2>
+      <header className={css.header}>
+        <h2 className={css.header__title}>Generate your QR code</h2>
+        <h3 className={css['header__sub-title']}>
+          Adjust the settings below to make your QR code look as you want
+        </h3>
       </header>
 
       <form className={css.form} onSubmit={handleSubmit}>
-        <div className={css['form-field']}>
-          <label htmlFor="data">Text / URL</label>
-          <span className="sub-label">
+        <div className={css.field}>
+          <label className={css.field__label} htmlFor="data">
+            Text / URL:
+          </label>
+          <span className={css['field__sub-label']}>
             Include text or a URL you want the QR code to lead to
           </span>
           <input
             name="data"
             id="data"
+            type="text"
             placeholder="Your text or URL"
             maxLength={900}
             onChange={handleChangeInput}
             required
           />
-          <span className="sub-label">{`${counter}/900`}</span>
+          <p className={css.field__value}>{`${counter}/900`}</p>
         </div>
 
-        <div className={css['form-field']}>
-          <label htmlFor="fgColor">Foreground Color</label>
-          <input
-            name="fgColor"
-            id="fgColor"
-            type="color"
-            value={formData.fgColor}
-            onChange={handleChangeInput}
-          />
-          <span className="sub-label">{formData.fgColor}</span>
-        </div>
-
-        <div className={css['form-field']}>
-          <label htmlFor="bgColor">Background Color</label>
-          <input
-            name="bgColor"
-            id="bgColor"
-            type="color"
-            value={formData.bgColor}
-            onChange={handleChangeInput}
-          />
-          <span className="sub-label">{formData.bgColor}</span>
-        </div>
-
-        <div className={css['form-field']}>
-          <label htmlFor="size">Size</label>
-          <div className="slider">
+        <div className={css.fieldset}>
+          <span className={css.fieldset__legend}>Colors:</span>
+          <div className={css.field}>
+            <label className={css.field__label} htmlFor="fgColor">
+              Foreground
+            </label>
             <input
-              type="range"
-              name="size"
-              id="size"
-              value={formData.size}
-              min="100"
-              max="1000"
-              step="50"
+              name="fgColor"
+              id="fgColor"
+              type="color"
+              value={formData.fgColor}
               onChange={handleChangeInput}
             />
+            <span className={css.field__value}>{formData.fgColor}</span>
           </div>
-          <span className="sub-label">
+
+          <div className={css.field}>
+            <label className={css.field__label} htmlFor="bgColor">
+              Background
+            </label>
+            <input
+              name="bgColor"
+              id="bgColor"
+              type="color"
+              value={formData.bgColor}
+              onChange={handleChangeInput}
+            />
+            <span className={css.field__value}>{formData.bgColor}</span>
+          </div>
+        </div>
+
+        <div className={css.field}>
+          <label className={css.field__label} htmlFor="size">
+            Size:
+          </label>
+          <input
+            type="range"
+            name="size"
+            id="size"
+            value={formData.size}
+            min="100"
+            max="1000"
+            step="50"
+            onChange={handleChangeInput}
+          />
+          <span className={css.field__value}>
             {`${formData.size}px * ${formData.size}px`}
           </span>
         </div>
 
-        <div className={css['form-field']}>
-          <label htmlFor="border">Border</label>
-          <div className="slider">
-            <input
-              type="range"
-              name="border"
-              id="border"
-              min="0"
-              max="10"
-              value={formData.border}
-              onChange={handleChangeInput}
-            />
-          </div>
-          <span className="sub-label">{`${formData.border} px`}</span>
+        <div className={css.field}>
+          <label className={css.field__label} htmlFor="border">
+            Border:
+          </label>
+
+          <input
+            type="range"
+            name="border"
+            id="border"
+            min="0"
+            max="10"
+            value={formData.border}
+            onChange={handleChangeInput}
+          />
+
+          <span className={css.field__value}>{`${formData.border} px`}</span>
         </div>
 
-        <button
-          className={css.cta}
-          id="cta"
-          type="submit"
-          disabled={isDisabled}
-        >
-          Generate QR code
+        <button className={css.cta} type="submit" disabled={isDisabled}>
+          <span>Generate QR code</span>
         </button>
       </form>
-      {qrCodeUrl && (
-        <div>
-          <img src={qrCodeUrl} alt="QR Code" />
-        </div>
-      )}
     </section>
   );
 };
